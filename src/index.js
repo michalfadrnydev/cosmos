222ffd050a68539324f43f3d06357c1be7967d25
Michal Fadrný <michal.fadrny.dev@gmail.com>
2023-08-29 14:09:50 +0200

    FaviconEdit

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ import "./styles/main.scss"
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Determine the theme mode
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+// Update favicon based on theme
+const link = document.querySelector("link[rel~='icon']");
+if (link) {
+  const faviconPath = prefersDarkMode ? "/favicon-dark.png" : "/favicon-light.png";
+  link.href = process.env.PUBLIC_URL + faviconPath;
+}
+
+// Listen for theme changes and update favicon accordingly
+window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
+  const newSchemeIsDark = e.matches;
+  const faviconPath = newSchemeIsDark ? "/favicon-dark.png" : "/favicon-light.png";
+  link.href = process.env.PUBLIC_URL + faviconPath;
+});
+
 root.render(
   <React.StrictMode>
     <App />
